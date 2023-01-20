c2876ac9456b9a3cbfa950578d16d0edf516e0c0
Eyre Hong <sentsuki8@gmail.com>
2023-01-19 23:44:07 -0800

    add fucntion docstring and edit contributing file

---
+++ R/find_old_name.R
@@ -0,0 +1,14 @@
+#' Generate the a random set of 10 suggested neutral(by default) baby names based on the given time period and sex.
+#'
+#' @param tp A character vector that specifying time period from 1880 to 2018
+#' @param limit A double vector that is the number of name in the output list. Default 10
+#' @param sex A character vector that is the aimed sex of the name in the output. Default "uni"
+#'
+#' @return A list contains random suggested old names based on the given limitation.
+#' @export
+#'
+#' @examples
+#' find_old_name('80s')
+find_old_name <- function(tp, limit=10, sex="uni") {
+  TRUE
+}
---
+++ R/find_unisex_name.R
@@ -0,0 +1,12 @@
+#' Generate the a random set of 10 suggested neutral baby names based on the given limitation and baby names in the past years.
+#'
+#' @param limit A double vector that is the minimum proportion of the neutral names in a single year in the data frame. Default 0.2
+#'
+#' @return A list contains random 10 suggested neutral names based on the given limitation
+#' @export
+#'
+#' @examples
+#' find_unisex_name(0.0001)
+find_unisex_name <- function(limit=0.2) {
+  TRUE
+}
